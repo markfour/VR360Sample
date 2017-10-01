134b4cf24bc311f2c3a543e38e2f98635f2f9ed1
markfour <axhfaro@gmail.com>
2017-10-01 22:37:47 +0900

    :up: Initialize

---
+++ VR360Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VR360Sample
+//
+//  Created by kazumi hayashida on 2017/10/01.
+//  Copyright © 2017年 organization name. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
