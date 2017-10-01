7ef39916feb3612f2d9871b3ffcc7301d01fc89a
markfour <axhfaro@gmail.com>
2017-10-02 00:53:22 +0900

    Delete pods file

--- Pods/Target Support Files/Pods-VR360Sample/Pods-VR360Sample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VR360Sample : NSObject
-@end
-@implementation PodsDummy_Pods_VR360Sample
-@end
--- Pods/Target Support Files/Pods-VR360SampleTests/Pods-VR360SampleTests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_VR360SampleTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_VR360SampleTestsVersionString[];
-
